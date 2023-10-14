75a9ebfea15047e0ce3ebaac63fe51ccdb648a9c
Fede Gilles <fgilles@ad-cap.com.ar>
2023-10-14 20:04:21 -0300

    home services

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 import partnerToto from '../../public/partner-toto.svg'
+import serviceBranding from '../../public/service-branding.svg'
 
 export default function Home() {
   return (
@@ -23,7 +24,57 @@ export default function Home() {
           </Link>
         </div>
       </header>
-      
+
+      <main>
+        <div className='homeServices'>
+          <div className='serviceTitle'>
+            <p>nuestros servicios</p>
+            <h3>per<span> </span>ro que ladra, <br /> mueerde</h3>
+          </div>
+
+          <div className='serviceBody'>
+            <div className='service'>
+              <Image src={serviceBranding}
+                alt="servicio Branding & Diseño"/>
+              
+              <div>
+                <h4>Branding & Diseño</h4>
+                <p>No hacemos logos, creamos marcas únicas. Le damos identidad a tu negocio y su propia historia. También creamos contenido para Social Media y pauta digital.</p>
+              </div>
+            </div>
+
+            <div className='service'>
+              <Image src={serviceBranding}
+                alt="servicio Branding & Diseño"/>
+              
+              <div>
+                <h4>Branding & Diseño</h4>
+                <p>No hacemos logos, creamos marcas únicas. Le damos identidad a tu negocio y su propia historia. También creamos contenido para Social Media y pauta digital.</p>
+              </div>
+            </div>
+
+            <div className='service'>
+              <Image src={serviceBranding}
+                alt="servicio Branding & Diseño"/>
+              
+              <div>
+                <h4>Branding & Diseño</h4>
+                <p>No hacemos logos, creamos marcas únicas. Le damos identidad a tu negocio y su propia historia. También creamos contenido para Social Media y pauta digital.</p>
+              </div>
+            </div>
+
+            <div className='service'>
+              <Image src={serviceBranding}
+                alt="servicio Branding & Diseño"/>
+              
+              <div>
+                <h4>Branding & Diseño</h4>
+                <p>No hacemos logos, creamos marcas únicas. Le damos identidad a tu negocio y su propia historia. También creamos contenido para Social Media y pauta digital.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
