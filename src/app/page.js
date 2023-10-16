e6aedf98b976365e3d2ab8aceaa67225979de82a
Fede Gilles <fgilles@ad-cap.com.ar>
2023-10-15 22:23:23 -0300

    new home sections

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,18 @@ import Image from 'next/image'
 
 import partnerToto from '../../public/partner-toto.svg'
 import serviceBranding from '../../public/service-branding.svg'
+import serviceSemSeo from '../../public/service-sem&seo.svg'
+import serviceSocial from '../../public/service-social.svg'
+import serviceUxUi from '../../public/service-uxui.svg'
+
+import clientArcadia from '../../public/client-arcadia.png'
+import clientIntergya from '../../public/client-intergya.png'
+import clientLatin from '../../public/client-latincloud.png'
+import clientMelon from '../../public/client-melonHelp.png'
+import clientTienda from '../../public/client-tiendanube.png'
+
+import businessEnhancers from '../../public/businessEnhancers.png'
+
 
 export default function Home() {
   return (
@@ -39,39 +51,88 @@ export default function Home() {
               
               <div>
                 <h4>Branding & Diseño</h4>
-                <p>No hacemos logos, creamos marcas únicas. Le damos identidad a tu negocio y su propia historia. También creamos contenido para Social Media y pauta digital.</p>
+                <p>Creamos marcas únicas, le damos identidad a tu negocio y su historia. También hacemos contenido para Social Media y pauta digital.</p>
               </div>
             </div>
 
             <div className='service'>
-              <Image src={serviceBranding}
-                alt="servicio Branding & Diseño"/>
+              <Image src={serviceSocial}
+                alt="servicio Social Media"/>
               
               <div>
-                <h4>Branding & Diseño</h4>
-                <p>No hacemos logos, creamos marcas únicas. Le damos identidad a tu negocio y su propia historia. También creamos contenido para Social Media y pauta digital.</p>
+                <h4>Social Media</h4>
+                <p>Podés elegir cualquiera de nuestros servicios y solicitar una consultoría personalizada según la necesidad de tu proyecto.</p>
               </div>
             </div>
 
             <div className='service'>
-              <Image src={serviceBranding}
-                alt="servicio Branding & Diseño"/>
+              <Image src={serviceSemSeo}
+                alt="servicio SEM & SEO"/>
               
               <div>
-                <h4>Branding & Diseño</h4>
-                <p>No hacemos logos, creamos marcas únicas. Le damos identidad a tu negocio y su propia historia. También creamos contenido para Social Media y pauta digital.</p>
+                <h4>SEM & SEO</h4>
+                <p>Gestionamos y moderamos los perfiles de tu marca en redes sociales a través de estrategias de contenido y atención al cliente.</p>
               </div>
             </div>
 
             <div className='service'>
-              <Image src={serviceBranding}
-                alt="servicio Branding & Diseño"/>
+              <Image src={serviceUxUi}
+                alt="servicio UX & UI"/>
               
               <div>
-                <h4>Branding & Diseño</h4>
-                <p>No hacemos logos, creamos marcas únicas. Le damos identidad a tu negocio y su propia historia. También creamos contenido para Social Media y pauta digital.</p>
+                <h4>UX/UI & Desarrollo</h4>
+                <p>Hacemos que tu plataforma o website se convierta en una experiencia intuitiva y única que te permita conectar con tus clientes.</p>
               </div>
             </div>
+          </div>
+        </div>
+
+        <div className='homeClients'>
+          <div className='clientsTitle'>
+            <p>nuestros clientes</p>
+            <h3>confían en <br/> nosootros</h3>
+          </div>
+
+          <div className='clientsCarousel'>
+            <Image src={clientArcadia}
+              alt="cliente"/>
+
+            <Image src={clientIntergya}
+              alt="cliente"/>
+
+            <Image src={clientLatin}
+              alt="cliente"/>
+
+            <Image src={clientMelon}
+              alt="cliente"/>
+
+            <Image src={clientTienda}
+              alt="cliente"/>
+          </div>
+        </div>
+
+        <div className='homeBusiness'>
+          <div className='businessTitle'>
+            <p>nosotros</p>
+            <h3>businnes<span> </span>s<br/> enhancers</h3>
+          </div>
+
+          <Image src={businessEnhancers}
+            alt="Perro founders"/>
+        </div>
+
+        <div className='homeContact'>
+          <div className='contactForm'>
+          </div>
+
+          <div className='contactCopy'>
+            <h3>
+              compartinos tus ideeas
+            </h3>
+
+            <p>
+              Ponete en contacto con nosotros y preparate para que llevemos tus ideas al siguiente nivel.
+            </p>
           </div>
         </div>
       </main>
