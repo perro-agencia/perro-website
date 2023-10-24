0e87850729e85f06de25d3b08c3b8388cdcd5ae3
Fede Gilles <fgilles@ad-cap.com.ar>
2023-10-23 22:41:53 -0300

    fixes & contact form start

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,10 +2,27 @@ export default function ContactPage() {
 
   return (
     <div className='contactBody'>
-      <div>
+      <div className="contactForm">
+        <form>
+          <div>
+            <input type="email" placeholder="Email" />
+            <input type="text" placeholder="Nombre" />
+          </div>
+
+          <div class="form-element">
+            <input type="text" placeholder="Empresa" />
+            <input type="number" placeholder="Teléfono" />
+          </div>
+
+          <div class="form-element">
+            <textarea id="" cols="30" rows="10" placeholder="Mensaje"></textarea>
+          </div>
+
+          <button type="submit">Enviar</button>
+        </form>
       </div>
 
-      <div>
+      <div className="contactCopy">
         <h2>
           contannos tus ideeas
         </h2>
