30ba809896935f1c6f4727a1d2a734c6cccae1a0
Fede Gilles <138924800+FedePerroAgency@users.noreply.github.com>
2024-03-21 00:23:56 -0300

    css adjustments

--- src/app/font.js
+++ src/app/font.js
@@ -2,5 +2,6 @@ import { Work_Sans as workSans } from 'next/font/google';
 
 export const font = workSans({
   subsets: ['latin'],
-  weight: ['100','200','300','400','500','600','700','800','900',]
+  weight: ['100','200','300','400','500','600','700','800','900',],
+  subsets: ['latin'],
 })
