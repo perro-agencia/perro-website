9c7738c305f4adaff8f527ba0e19cafe059acf30
Fede Gilles <federicogilles@perroagency.com>
2024-04-05 18:54:28 -0300

    meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,8 +17,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <>
-    <html lang="en">
+      <Head>
+        <html lang="en" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WJMXBSP');
+            `,
+          }}
+        />
+      </Head>
       <body className={font.className}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WJMXBSP"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <div id='top'></div>
         <Nav id="nav"/>
         {children}
@@ -33,7 +48,6 @@ export default function RootLayout({ children }) {
         
         <Footer />
       </body>
-    </html>
     </>
   )
 }
