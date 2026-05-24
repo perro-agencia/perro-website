622d0e166ff0b7b06059b1dad6538d50e5459033
Fede Gilles <138924800+FedePerroAgency@users.noreply.github.com>
2026-05-24 11:59:42 -0300

    ambient blob test delete

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -3,10 +3,10 @@ import { Footer } from "@/components/layout/Footer"
 
 export default function SiteLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="relative z-10">
       <Header />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   )
 }
